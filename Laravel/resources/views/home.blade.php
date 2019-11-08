2d0f14f896e9cc59d38f5ee4241261b797ac9eef
MIGUEL <miguelantiman.mav@gmail.com>
2019-11-08 19:57:31 -0300

    maquetanto view de videos

--- Laravel/resources/views/home.blade.php
+++ Laravel/resources/views/home.blade.php
@@ -3,33 +3,30 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="container">
             @if(session('message'))
                 <div class="alert alert-success">
                     {{ session('message') }}
                 </div>
             @endif
-
-            <div class="row">
-            @foreach($videos as $video)
-                <div class="col-4 my-3">
-                    <div class="card av-card" style="width: 18rem;">
-                        @if(Storage::disk('images')->has($video->image))
-                        <img src="{{url('/miniatura/'.$video->image)}}" class="card-img-top" alt="...">
-                        @endif
-                        <div class="card-body">
-                            <h4>{{ $video->title }}</h4>
-                        </div>
-                        <div class="card-footer">
-                            <button type="button" class="btn btn-info btn-block">ver video</button>
-                        </div>
-                    </div>              
-                </div>
-            @endforeach  
-            </div>
-        </div>
-
-        {{ $videos->links() }}
     </div>
+    <div class="row">
+    @foreach($videos as $video)
+        <div class="col-12 col-sm-6 col-md-4 my-3">
+            <div class="card col-sm-12" >
+                @if(Storage::disk('images')->has($video->image))
+                <img src="{{url('/miniatura/'.$video->image)}}" class="card-img-top mt-3 " alt="...">
+                @endif
+                <div class="card-body">
+                    <h4>{{ $video->title }}</h4>
+                </div>
+                <div class="card-footer">
+                    <button type="button" class="btn btn-info btn-block">ver video</button>
+                </div>
+            </div>              
+        </div>
+    @endforeach  
+    </div>
+{{ $videos->links() }}
+
 </div>
 @endsection
